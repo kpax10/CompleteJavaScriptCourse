697d66684bbbd12ce815fb646000dc328ceffa71
Kyle <notxap55@yahoo.com>
2022-01-30 21:25:46 -0800

    continuing

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,23 @@
+//////////////////////////////////
+//Values and Variables
+const country = 'USA';
+const continent = 'North America';
+let population = 350000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+///////////////////////////////////
+//Data Types
+const isIsland = false;
+let language = 'English';
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+///////////////////////////////////
+//let, const, and var
+language = 'English';
+///////////////////////////////////
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,52 @@
+/*
+/////////////////////////////
+//Values and Variables
+let js = 'amazing';
+console.log(40 + 8);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'teacher';
+//////////////////////////////
+*/
+/*
+//////////////////////////////
+//Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+///////////////////////////////
+*/
+/*
+///////////////////////////////
+//let, const, and var
+let age = 30;
+age = 31;
+
+const birthYear = 1990;
+birthYear = 1991;
+
+const job;
+
+var job = 'programmer';
+job = 'teacher';
+///////////////////////////////
+*/
