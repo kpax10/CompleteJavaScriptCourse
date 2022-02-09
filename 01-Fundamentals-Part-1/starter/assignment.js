0e36dba4ddd8238a7847c954c0155b8b344de384
Kyle <notxap55@yahoo.com>
2022-02-08 22:12:40 -0800

    completed coding challenge 3

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -1,45 +1,45 @@
 //////////////////////////////////
 // Values and Variables
-const country = 'USA';
-const continent = 'North America';
-let population = 350000000;
+// const country = 'USA';
+// const continent = 'North America';
+// let population = 350000000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 ///////////////////////////////////
 // Data Types
-const isIsland = false;
-let language;
+// const isIsland = false;
+// let language;
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 ///////////////////////////////////
 // Let, Const, and Var
-language = 'English';
+// language = 'English';
 ///////////////////////////////////
 // Basic Operators
-console.log(population / 2);
-population++;
-console.log(population);
-console.log(population > 6000000);
-console.log(population < 33000000);
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// console.log(population > 6000000);
+// console.log(population < 33000000);
 
-const description = country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language + '.';
-console.log(description);
+// const description = country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language + '.';
+// console.log(description);
 ///////////////////////////////////
 // Strings and Template Literals
-const description1 = `${country} is in ${continent}, and its ${population} people speak ${language}.`
-console.log(description1);
-///////////////////////////////////
-// If / Else Statements
-if (population > 33000000) {
-    console.log(`${country}'s population is above average.`)
-} else {
-    console.log(`${country}'s population is ${33000000 - population} below average.`)
-}
+// const description1 = `${country} is in ${continent}, and its ${population} people speak ${language}.`
+// console.log(description1);
+// ///////////////////////////////////
+// // If / Else Statements
+// if (population > 33000000) {
+//     console.log(`${country}'s population is above average.`)
+// } else {
+//     console.log(`${country}'s population is ${33000000 - population} below average.`)
+// }
 ///////////////////////////////////
 // Type Conversion and Coercion
 // 4
@@ -50,10 +50,22 @@ if (population > 33000000) {
 ///////////////////////////////////
 // Equality Operators
 
-const numNeighbors = Number(prompt('How many neighbor countries does your country hav?'));
+// const numNeighbors = Number(prompt('How many neighbor countries does your country hav?'));
 
-if (numNeighbors === 1) {
-    console.log('Only 1 border');
-} else if (numNeighbors > 1) {
-    console.log('More than 1 border');
-} else console.log('No borders');
+// if (numNeighbors === 1) {
+//     console.log('Only 1 border');
+// } else if (numNeighbors > 1) {
+//     console.log('More than 1 border');
+// } else console.log('No borders');
+////////////////////////////////////
+// Logical Operators
+
+// const country = 'USA';
+// const language = 'English';
+// const population = 350;
+// const isIsland = false;
+
+// if (language === 'English' && population < 50 && !isIsland) {
+//     console.log(`You should live in ${country}!`)
+// } else console.log(`${country} does not meet your criteria`)
+//////////////////////////////////////
