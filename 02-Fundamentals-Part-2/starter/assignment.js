95456bc61b87095a4cf68fa7c809e8518ff6d700
Kyle <notxap55@yahoo.com>
2022-02-13 22:12:22 -0800

    started functions

---
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -0,0 +1,16 @@
+////////////////////////////
+/*
+// Functions
+'use strict';
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}.`
+}
+const descUsa = describeCountry('USA', 350, 'Washington, DC');
+const descPortugal = describeCountry('Portugal', 6, 'Lisbon');
+const descCanada = describeCountry('Canada', 37, 'Ottawa');
+
+console.log(descUsa, descPortugal, descCanada);
+//////////////////////////////
+*/
+
