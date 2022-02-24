d25eed5dc4128d32ee1e46e3de492f9ee185f6ea
Kyle <notxap55@yahoo.com>
2022-02-23 20:41:04 -0800

    for loops started

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -157,3 +157,11 @@ console.log(myCountry);
 console.log(myCountry.isIsland);
 /////////////////////////////////
 */
+/*
+// For Loops
+
+for (let voter = 1; voter <= 50; voter++) {
+    console.log(`Voter number ${voter} is currently voting`)
+}
+*/
+/////////////////////////////////
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -367,4 +367,22 @@ john.calcBMI();
 console.log(`${john.fullName}'s BMI (${john.bmi}) is ${john.bmi > mark.bmi ? 'higher' : 'lower'} than ${mark.fullName}'s (${mark.bmi})`);
 */
 ///////////////////////////////////
-// 
+/*
+// For Loop
+
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Lifting weights repetition ${i}`)
+}
+*/
+//////////////////////////////////
