249b8d5a6696c566902e05d759257adf5919ef3a
Kyle <notxap55@yahoo.com>
2022-02-28 21:57:29 -0800

    guess my number started

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,8 @@
 'use strict';
 
 console.log(document.querySelector('.message'));
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
