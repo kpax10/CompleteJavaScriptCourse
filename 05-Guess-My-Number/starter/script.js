842253c4f4ee5660345bbc8f77fb8380d68f89ea
Kyle <notxap55@yahoo.com>
2022-03-01 15:48:07 -0800

    added to guess number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,24 @@
+/*
 'use strict';
+
 
 console.log(document.querySelector('.message'));
 document.querySelector('.message').textContent = 'Correct Number!';
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+'use strict';
 
+const number = Math.floor(Math.random() * 20) + 1;
+console.log(number);
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No number'
+    }
+    if (guess === number) console.log('you win');
+});
