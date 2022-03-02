350cfdb21f490c1933664a42ab0b1313dac72bf3
Kyle <notxap55@yahoo.com>
2022-03-02 07:05:52 -0800

    added changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,15 +13,29 @@ console.log(document.querySelector('.guess').value);
 const secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+// Reset button
+document.querySelector('.again').addEventListener('click', function () {
+    document.querySelector('.message').textContent = 'Start guessing...'
+    // reset background color
+    // reset score back to 20
+    // reset ? block size and number back to '?'
+})
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
+    // When there is no input
     if (!guess) {
         document.querySelector('.message').textContent = 'No Number...'
+
+        // When player wins
     } else if (guess === secretNumber) {
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('body').style.backgroundColor = ' #60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        // When guess is too high
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too high!';
@@ -31,6 +45,8 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.message').textContent = 'You lose the game';
             document.querySelector('.score').textContent = 0;
         }
+
+        // When guess is too low
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too low!';
