14aed14bb747bb95d152e376200ad9a0c6a5ba68
Kyle <notxap55@yahoo.com>
2022-03-01 22:05:27 -0800

    implement core functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,15 +10,35 @@ console.log(document.querySelector('.guess').value);
 */
 'use strict';
 
-const number = Math.floor(Math.random() * 20) + 1;
-console.log(number);
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
 
-document.querySelector('.check').addEventListener('click', function() {
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
 
-    if(!guess) {
-        document.querySelector('.message').textContent = 'No number'
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number...'
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!';
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too high!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lose the game';
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lose the game';
+            document.querySelector('.score').textContent = 0;
+        }
     }
-    if (guess === number) console.log('you win');
 });
