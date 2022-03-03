b5f6ca0f8dbd2c2c9156bce252214614c45b8b1a
Kyle <notxap55@yahoo.com>
2022-03-02 20:13:22 -0800

    game complete

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,16 +10,11 @@ console.log(document.querySelector('.guess').value);
 */
 'use strict';
 
-const secretNumber = Math.floor(Math.random() * 20) + 1;
+let secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
-// Reset button
-document.querySelector('.again').addEventListener('click', function () {
-    document.querySelector('.message').textContent = 'Start guessing...'
-    // reset background color
-    // reset score back to 20
-    // reset ? block size and number back to '?'
-})
+
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -34,6 +29,11 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.message').textContent = 'Correct Number!';
         document.querySelector('body').style.backgroundColor = ' #60b347';
         document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
 
         // When guess is too high
     } else if (guess > secretNumber) {
@@ -58,3 +58,17 @@ document.querySelector('.check').addEventListener('click', function () {
         }
     }
 });
+
+// Reset button
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+
+    secretNumber = Math.floor(Math.random() * 20) + 1;
+    // reroll secretNumber
+})
