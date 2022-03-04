ade0767e9fbb51861bcf08b1eef4c9f3d2bf0f8d
Kyle <notxap55@yahoo.com>
2022-03-04 15:21:13 -0800

    functionality added, not optimized

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,7 +2,23 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.closeModal');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', function () {
+        console.log(btnsOpenModal[i].textContent);
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+};
 
+btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
