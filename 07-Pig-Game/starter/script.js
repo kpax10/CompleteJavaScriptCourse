2a70d054240b849d48376dadce7ecf0a7b4b9339
Kyle <notxap55@yahoo.com>
2022-03-06 22:27:20 -0800

    started logic to pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+document.querySelector('.btn--roll').addEventListener('click', function () {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    const imgDie = document.querySelector('img');
+
+    if (roll === 1) {
+        imgDie.src = 'dice-1.png'
+        return;
+    } else if (roll === 2) {
+        imgDie.src = 'dice-2.png'
+    } else if (roll === 3) {
+        imgDie.src = 'dice-3.png'
+    } else if (roll === 4) {
+        imgDie.src = 'dice-4.png'
+    } else if (roll === 5) {
+        imgDie.src = 'dice-5.png'
+    } else imgDie.src = 'dice-6.png'
+    return roll;
+});
+
+// if not a 1, add the roll to the current score
+// if it is a 1, switch players
