0854758343353bdbff434b290196b4cc83894dd1
Kyle <notxap55@yahoo.com>
2022-03-07 15:54:17 -0800

    updated dice roll and display added

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,37 +1,77 @@
 'use strict';
 
-const p1CurrentDOM = document.querySelector('#current--0')
-let p1Current = 0;
+// Selecting Elements
+const score0DOM = document.querySelector('#score--0');
+const score1DOM = document.querySelector('#score--1');
+const current0DOM = document.querySelector('#current--0');
+const current1DOM = document.querySelector('#current--1');
+const diceDOM = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
-document.querySelector('.btn--roll').addEventListener('click', function () {
-    const roll = Math.floor(Math.random() * 6) + 1;
-    const imgDie = document.querySelector('img');
 
-    if (roll === 1) {
-        imgDie.src = 'dice-1.png'
-        p1Current = 0;
-        // SWITCH USERS HERE
+// Reset scores and die
+score0DOM.textContent = 0;
+score1DOM.textContent = 0;
+diceDOM.classList.add('hidden');
 
-        // return;
-    } else if (roll === 2) {
-        imgDie.src = 'dice-2.png'
-        p1Current += roll;
+let currentScore = 0;
 
-    } else if (roll === 3) {
-        imgDie.src = 'dice-3.png'
-        p1Current += roll;
-    } else if (roll === 4) {
-        imgDie.src = 'dice-4.png'
-        p1Current += roll;
-    } else if (roll === 5) {
-        imgDie.src = 'dice-5.png'
-        p1Current += roll;
-    } else {
-        imgDie.src = 'dice-6.png'
-        p1Current += roll;
-    }
-    p1CurrentDOM.textContent = p1Current;
+// Rolling Dice Functionality
+
+btnRoll.addEventListener('click', function () {
+    //1. Generating a random dice roll
+    const dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Remove hidden class, Display the Dice roll
+    diceDOM.classList.remove('hidden');
+    const diceImg = document.querySelector('img');
+    diceImg.src = `dice-${dice}.png`;
+
+    //3. Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+        // add dice to current score
+        currentScore += dice;
+        current0DOM.textContent = currentScore;
+    } else
+        currentScore = 0;
+    current0DOM.textContent = currentScore;
+
+    // switch to next player
+    console.log(currentScore);
 });
+
+
+
+// const p1CurrentDOM = document.querySelector('#current--0')
+// let p1Current = 0;
+// let roll;
+
+// document.querySelector('.btn--roll').addEventListener('click', function () {
+//     const roll = Math.floor(Math.random() * 6) + 1;
+//     const imgDie = document.querySelector('img');
+
+//     if (roll === 1) {
+//         imgDie.src = 'dice-1.png'
+//     } else if (roll === 2) {
+//         imgDie.src = 'dice-2.png'
+//     } else if (roll === 3) {
+//         imgDie.src = 'dice-3.png'
+//     } else if (roll === 4) {
+//         imgDie.src = 'dice-4.png'
+//     } else if (roll === 5) {
+//         imgDie.src = 'dice-5.png'
+//     } else {
+//         imgDie.src = 'dice-6.png'
+//     }
+//     return roll;
+// });
+
+
+// p1Current = 0;
+// p1Current += roll;
+// p1CurrentDOM.textContent = p1Current;
 
 // if not a 1, add the roll to the current score
 // if it is a 1, switch players
