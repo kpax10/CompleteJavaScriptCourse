c45a2420b1efaa395a728d64005e64c40e91d657
Kyle <notxap55@yahoo.com>
2022-03-11 15:54:04 -0800

    scoping added

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,2 +1,34 @@
 'use strict';
 
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    console.log(firstName);
+
+    function printAge() {
+        let output = `${firstName}, You are ${age}, born in ${birthYear}`
+        console.log(age);
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            const firstName = 'Steven'
+            const str = `Oh, you're a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b) {
+                return a + b;
+            }
+
+            output = 'NEW OUTPUT';
+        }
+        // console.log(str);
+        console.log(millenial);
+        // console.log(add(2, 3));
+    };
+    printAge();
+
+    return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
