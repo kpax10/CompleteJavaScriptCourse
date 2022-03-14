f1ae3c896cd85a44ba43ae60b75ff40f9748a455
Kyle <notxap55@yahoo.com>
2022-03-14 11:47:54 -0700

    this started

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,3 +1,4 @@
+/*
 'use strict';
 
 function calcAge(birthYear) {
@@ -32,3 +33,46 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+*/
+/*
+'use strict';
+
+// Variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+const addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted');
+}
+
+
+var x = 1;
+let y = 2;
+const z = 3;
+*/
