1407afb3bc0a445a360a87a6340443491f1f3662
Kyle <notxap55@yahoo.com>
2022-03-14 21:29:49 -0700

    added to 'this'

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -76,3 +76,37 @@ var x = 1;
 let y = 2;
 const z = 3;
 */
+'use strict';
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//     console.log(2037 - birthYear);
+//     console.log(this);
+// }
+// calcAge(1986);
+
+const calcAgeArrow = (birthYear) => {
+    console.log(2037 - birthYear);
+    console.log(this);
+}
+calcAgeArrow(1986);
+
+const jonas = {
+    year: 1991,
+    calcAge: function () {
+        console.log(this);
+        console.log(2037 - this.year);
+    }
+}
+jonas.calcAge();
+
+const matilda = {
+    year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
