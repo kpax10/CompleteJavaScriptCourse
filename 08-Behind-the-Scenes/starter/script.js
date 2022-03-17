d3d6f9ce7e44ad2a9255054276c1b911d3e8944f
Kyle <notxap55@yahoo.com>
2022-03-16 21:26:28 -0700

    primitives and objects complete

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -150,3 +150,35 @@ addExpr(2, 5);
 
 var addArrow = (a, b) => a + b;
 */
+
+'use strict';
+
+// Primitive Types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference Types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis'
+console.log(jessica, marriedJessica);
+
+// Copying Objects
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alcie', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('andy');
+console.log(jessica2, jessicaCopy);
